b7171fefcfe9b9338d28348ea6266a5e9c3b0477
Scott Weiss <sdw35@cornell.edu>
2017-07-14 10:54:59 -0400

    add integration tests and run them with travis

--- hack/install-tools.sh
+++ hack/install-tools.sh
@@ -8,5 +8,7 @@ source "${CODE_ROOT}/hack/common.sh"
 echo "Detected go version: $(go version)"
 
 go get github.com/tools/godep
+go get github.com/onsi/ginkgo/ginkgo  # installs the ginkgo CLI
+go get github.com/onsi/gomega         # fetches the matcher library
 
 ret=$?; ENDTIME=$(date +%s); echo "$0 took $(($ENDTIME - $STARTTIME)) seconds"; exit "$ret"
